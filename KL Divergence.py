c30845067d3b6c389256c1a04b13e4447f5905a4
Collin Andrews <pizzaTrucks@outlook.com>
2025-01-30 13:56:38 -0600

    Update KL Divergence.py

--- KL Divergence.py
+++ KL Divergence.py
@@ -1,36 +1,79 @@
-def KLDivergence(self, P, Q, printProb, fixedSeq, scaler):
-    print('===================================== KL Divergence '
-          '=====================================')
+# Calculate the Kullback–Leibler (KL) divergence
+
+import numpy as np
+import pandas as pd
+
+
+
+# ===================================== Create Data ======================================
+probDist1 = pd.DataFrame(np.random.rand(5, 5),
+                         index=['A', 'B', 'C', 'D', 'E'],
+                         columns=['V', 'W', 'X', 'Y', 'Z'])
+probDist1 = probDist1.div(probDist1.sum(axis=0), axis=1) # Normalize values
+probDist2 = pd.DataFrame(np.random.rand(5, 5),
+                         index=['A', 'B', 'C', 'D', 'E'],
+                         columns=['V', 'W', 'X', 'Y', 'Z'])
+probDist2 = probDist2.div(probDist2.sum(axis=0), axis=1)
+probScaler = pd.DataFrame([1.21, 0.87, 2.37, 0.98, 0.54])
+probLabel = 'Random Dataset'
+
+
+
+# =================================== Define Function ====================================
+def KLDivergence(P, Q, datasetTag, scaler):
+    print('================================= KL Divergence '
+          '=================================')
     P.columns = Q.columns
-    if printProb:
-        print(f'Baseline Probability Distribution:\n{Q}\n\n\n'
-              f'Probability Distribution:\n{P}\n\n')
+    print(f'True Probability Distribution:\n'
+          f'{P}\n\n'
+          f'Model Probability Distribution:\n'
+          f'{Q}\n\n')
 
-    divergence = pd.DataFrame(0, columns=Q.columns, index=[fixedSeq], dtype=float)
-    divergenceMatrix = pd.DataFrame(0, columns=Q.columns, index=Q.index, dtype=float)
+    # modelDistize: Dataframes
+    divergence = pd.DataFrame(0, 
+                              columns=Q.columns, 
+                              index=[datasetTag], 
+                              dtype=float)
+    divergenceMatrix = pd.DataFrame(0, 
+                                    columns=Q.columns, 
+                                    index=Q.index, 
+                                    dtype=float)
 
-    for position in Q.columns:
-        p = P.loc[:, position]
-        q = Q.loc[:, position]
-        divergence.loc[fixedSeq, position] = (
+    # Calculate: Divergence
+    for column in Q.columns:
+        p = P.loc[:, column]
+        q = Q.loc[:, column]
+        divergence.loc[datasetTag, column] = (
             np.sum(np.where(p != 0, p * np.log2(p / q), 0)))
 
-        for residue in Q.index:
-            initial = Q.loc[residue, position]
-            final = P.loc[residue, position]
-            if initial == 0 or final == 0:
-                divergenceMatrix.loc[residue, position] = 0
+        for row in Q.index:
+            trueDist = P.loc[row, column]
+            modelDist = Q.loc[row, column]
+
+            # Do not allow NaN
+            if modelDist == 0 or trueDist == 0:
+                divergenceMatrix.loc[row, column] = 0 # Set NaN to 0
             else:
-                divergenceMatrix.loc[residue, position] = final * np.log2(final / initial)
+                divergenceMatrix.loc[row, column] = (trueDist * 
+                                                     np.log2(trueDist / modelDist))
 
     # Scale the values
     if scaler is not None:
-        for position in Q.columns:
-            divergenceMatrix.loc[:, position] = (divergenceMatrix.loc[:, position] *
-                                                 scaler.loc[position, 'ΔEntropy'])
+        for index, column in enumerate(Q.columns):
+            divergenceMatrix.loc[:, column] = (divergenceMatrix.loc[:, column] *
+                                               scaler.iloc[index, 0])
 
-    print(f'{silver}KL Divergence:{pink} Fixed Final Sort - {fixedSeq}{resetColor}\n'
-          f'{divergence}\n\n\n{silver}Divergency Matrix:{pink} Fixed Final Sort - {fixedSeq}'
-          f'{resetColor}\n{divergenceMatrix.round(4)}\n\n')
+    print(f'KL Divergence: {datasetTag}\n'
+          f'{divergence}\n\n\n'
+          f'Divergency Matrix: {datasetTag}'
+          f'\n{divergenceMatrix.round(4)}\n\n')
 
     return divergenceMatrix, divergence
+
+
+
+# ==================================== Evaluate Data =====================================
+KLDivergence(P=probDist1,
+             Q=probDist2,
+             datasetTag=probLabel,
+             scaler=probScaler)

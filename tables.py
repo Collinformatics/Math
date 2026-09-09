2d01ebd00a144739bc305de53e9cfe2c6710bb18
Collin Andrews <179949844+Collinformatics@users.noreply.github.com>
2026-09-09 14:16:53 -0500

    Add files via upload

--- tables.py
+++ tables.py
@@ -10,12 +10,12 @@ import sys
 """
 
 parser = argparse.ArgumentParser(description='Mathematics Tables')
-parser.add_argument('-d', '--difficult', action='store_true',
-                       help='Increase difficulty by skipping the 5s and 10s')
+parser.add_argument('-r', '--randomize', action='store_true',
+                       help='Increase difficulty by randomizing the order of values')
 parser.add_argument('-m', '--maxvalue', type=int, default=15,
                     help='Maximal value for the table')
 args = parser.parse_args()
-hardMode = args.difficult
+randomize = args.randomize
 N = args.maxvalue
 
 # Colors
@@ -28,21 +28,16 @@ def deleteLine(nLines=1):
     print('\033[F\033[2K' * nLines, end='') # Move cursor up & delete
 
 
-def getValues(valueSet, maxValue=15, hard=False):
+def getValues(valueSet, maxValue=15):
     a = [v for v in range(3, maxValue+1, 1)]
     b = [v for v in range(3, 17, 1)]
-    if valueSet == 'multiply':
-        if hard:
-            # Remove 5 & 10
-            a = a[(a != 5) & (a != 10)]
-            b = b[(b != 5) & (b != 10)]
 
     return a, len(str(a[-1])), b, len(str(b[-1]))
 
 
-def multiplication():
+def multiplication(hard=False):
     print('Multiplication Tables:')
-    val, lenV, col, lenC = getValues(valueSet='multiply', maxValue=N, hard=hardMode)
+    val, lenV, col, lenC = getValues(valueSet='multiply', maxValue=N)
     for _ in range(len(val)-1):
         # Select value
         i = random.randint(1, len(val)-1)
@@ -52,6 +47,8 @@ def multiplication():
         print(f'\nValue: {pink}{v}{rst}')
 
         # Test
+        if hard:
+            random.shuffle(col)
         for c in col:
             value = v * c
             spaceC = " " * (lenC - len(str(c)))
@@ -68,24 +65,25 @@ def multiplication():
     print()
 
 
-def division():
+def division(hard=False):
     print('Division Tables:')
-    div, lenD, num, _ = getValues(valueSet='divide', maxValue=N, hard=hardMode)
+    div, lenD, num, _ = getValues(valueSet='divide', maxValue=N, hard=randomize)
     for _ in range(len(div)-1):
         # Select value
         i = random.randint(1, len(div)-1)
         d = div[i]
         div = np.delete(div, i)
         spaceD = " " * (lenD - len(str(d)))
-        random.shuffle(num)
-        factors = [d * n for n in num]
-        lenN = len(str(factors[-1]))
-        # if not any(n % d == 0 and n/d != 1.0  for n in num):
-        #     continue
         print(f'\nValue: {pink}{d}{rst}')
 
+        # Determine values
+        if hard:
+            random.shuffle(num)
+        values = [d * n for n in num]
+        lenN = len(str(values[-1]))
+
         # Test
-        for n in factors:
+        for n in values:
             value = n / d
             if value == 1.0 or int(value) != value:
                 # print(f'* {d}, {n}, {value}')
@@ -111,9 +109,10 @@ def question():
           '1: Division')
     x = input('Enter value: ')
     if x in tables.keys():
+        print()
         return tables[x]
     else:
-        deleteLine()(4)
+        deleteLine(4)
         return question()
 
 
@@ -122,6 +121,6 @@ if __name__ == '__main__':
         # exercise = 'multiply'
         exercise = question()
         if exercise == 'multiply':
-            multiplication()
+            multiplication(randomize)
         elif exercise == 'divide':
-            division()
+            division(randomize)

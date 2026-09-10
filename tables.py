70d356d412ce337b41c2ac709084875321f78537
Collin Andrews <179949844+Collinformatics@users.noreply.github.com>
2026-09-09 19:31:02 -0500

    Add files via upload

--- tables.py
+++ tables.py
@@ -5,8 +5,8 @@ import readline
 import sys
 
 """
-    Master fundamentals the of mathematics by practicing the 
-    multiplication & division tables
+    Master the fundamentals of mathematics by practicing the 
+    multiplication & division tables, percentages, and fractions
 """
 
 parser = argparse.ArgumentParser(description='Mathematics Tables')
@@ -32,20 +32,29 @@ def deleteLine(nLines=1):
     print('\033[F\033[2K' * nLines, end='') # Move cursor up & delete
 
 
-def pBar(l=50):
+def printBar(l=50):
     print('*'*l)
+    
+
+def printError(msg):
+    print(f'{red}{msg}{rst}')
 
 
-def getValues(maxValue=15):
-    a = [v for v in range(3, maxValue+1, 1)]
-    b = [v for v in range(3, 17, 1)]
-
+def getValues(problemSet, maxValue=15):
+    a, b = [], []
+    if problemSet == 'multiplication' or problemSet == 'division':
+        a = [v for v in range(3, maxValue+1, 1) if v not in [10]]
+        b = [v for v in range(3, 17, 1)]
+    elif problemSet == 'percentage':
+        a = [v for v in range(100, 400, 50)]
+        b = [v / 100 for v in range(10, 100, 10)]
+    # print(f'a: {a}\nb: {b}')
     return a, len(str(a[-1])), b, len(str(b[-1]))
 
 
 def multiplication(hard=False):
     print('Multiplication Tables:')
-    val, lenV, col, lenC = getValues(maxValue=N)
+    val, lenV, num, lenN = getValues(problemSet='multiplication', maxValue=N)
     for _ in range(len(val)-1):
         # Select value
         i = random.randint(1, len(val)-1)
@@ -56,26 +65,26 @@ def multiplication(hard=False):
 
         # Test
         if hard:
-            random.shuffle(col)
-        for c in col:
-            value = v * c
-            spaceC = " " * (lenC - len(str(c)))
+            random.shuffle(num)
+        for n in num:
+            value = v * n
+            spaceN = " " * (lenN - len(str(n)))
             while True:
-                x = input(f'  {spaceV}{v} x {spaceC}{c} = ')
+                x = input(f'  {spaceV}{v} x {spaceN}{n} = ')
                 if x:
                     if float(x) == value:
                         break
                     else:
                         deleteLine()
-                        print(f'  {red}{spaceV}{v} x {spaceC}{c} = {x}{rst}')
+                        printError(f'  {spaceV}{v} x {spaceN}{n} = {x}')
                 else:
                     deleteLine()
-    pBar()
+    printBar()
 
 
 def division(hard=False):
     print('Division Tables:')
-    div, lenD, num, _ = getValues(maxValue=N)
+    div, lenD, num, _ = getValues(problemSet='division', maxValue=N)
     for _ in range(len(div)-1):
         # Select value
         i = random.randint(1, len(div)-1)
@@ -104,20 +113,54 @@ def division(hard=False):
                         break
                     else:
                         deleteLine()
-                        print(f'  {red}{spaceN}{n} / {d}{spaceD} = {x}{rst}')
+                        printError(f'  {spaceN}{n} / {d}{spaceD} = {x}')
                 else:
                     deleteLine()
-    pBar()
+    printBar()
+
+
+def percentage(hard=False):
+    print('Percentage:')
+    val, lenV, per, lenP = getValues(problemSet='percentage', maxValue=N)
+    for _ in range(len(val)-1):
+        # Select value
+        i = random.randint(1, len(val)-1)
+        v = val[i]
+        val = np.delete(val, i)
+        spaceV = " " * (lenV - len(str(v)))
+        print(f'\nValue: {pink}{v}{rst}')
+
+        # Determine values
+        percents = [p for p in per if (v * p) % 1 == 0]
+        # print(f'Per: {pink}{percents}{rst}')
+        if hard:
+            random.shuffle(percents)
+
+        # Test
+        for p in percents:
+            pInt = int(p*100)
+            value = v * p
+            spaceP = " "
+            while True:
+                x = input(f'  {pInt}% of {v} = ')
+                if x:
+                    if float(x) == round(value, 2):
+                        break
+                    else:
+                        deleteLine()
+                        printError(f'  {pInt}% of {v} = {value}')
+                else:
+                    deleteLine()
 
 
 def question():
-    tables = {'1': 'multiply', '2': 'divide'}
+    tables = {'1': 'multiply', '2': 'divide', '3': 'percentage'}
     print('Select Exercise:')
     for k, v in tables.items():
         print(f'{k}: {v}')
     x = input('Enter value: ')
     if x in tables.keys() or x in tables.values():
-        pBar()
+        printBar()
         return tables[x]
     else:
         deleteLine(4)
@@ -132,3 +175,5 @@ if __name__ == '__main__':
             multiplication(randomize)
         elif exercise == 'divide':
             division(randomize)
+        elif exercise == 'percentage':
+            percentage(randomize)
